c25af36dacee704bbf7b8e8a5bbd95f871cb8a27
khaingminntun <125485807+khaingminntun@users.noreply.github.com>
2024-02-12 11:46:43 +0630

    Update php_action.php

--- php_action.php
+++ php_action.php
@@ -3,9 +3,9 @@
 require_once 'db_connect.php';
 
 if ($_POST) {
-    $submit = $_POST['text'];
-    $sql = "INSERT INTO submitform(submitText) VALUES ('$submit')";
-    if ($connect-> query($sql) === TRUE) {
+    $submit = $_POST['submit_te'];
+    $sql = "INSERT INTO submitform (submitText) VALUES ('$submit')";
+    if ($connect->query($sql) === TRUE) {
         echo "<p>Thanks for Sumitting!</p>";
     }
 }
