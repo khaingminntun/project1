bd86eb12ecb553447a566a9e91d0604c6bbeb90e
khaingminntun <125485807+khaingminntun@users.noreply.github.com>
2024-02-12 11:35:45 +0630

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,10 @@
+<?php
+$localhost = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "birthday_wish";
+
+// create connection
+$connect = new mysqli($localhost, $username, $password, $dbname);
+
+?>
